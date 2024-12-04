63353d203264f9ac0e283da06a2944921a11c012
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2024-12-04 14:58:16 +0530

    updated

--- components/Email.jsx
+++ components/Email.jsx
@@ -98,12 +98,69 @@ const Email = () => {
               required
             />
           </div>
-          <button
+          {/* <button
             className="bg-gray-900 hover:bg-orange-600 text-white font-bold py-2 px-4 w-40 h-12 mt-6 md:mt-8"
             type="submit"
           >
             Send
-          </button>
+          </button> */}
+          <div className="flex flex-col justify-between">
+            <div>
+              <button
+                className="bg-gray-900 hover:bg-orange-600 text-white font-bold py-2 px-4 w-40 h-12 mt-6 md:mt-8"
+                type="submit"
+              >
+                Send
+              </button>
+            </div>
+            <div className="flex flex-row items-center justify-center gap-4">
+              <div className="flex justify-center mt-6 md:mt-8 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 mr-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <a
+                  href="tel:+1234567890"
+                  className="text-orange-600 hover:text-orange-700 font-semibold"
+                >
+                  Call
+                </a>
+              </div>
+
+              <div className="flex justify-center mt-6 md:mt-8 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                  />
+                </svg>
+                <a
+                  href="mailto:harshibagla30@gmail.com"
+                  className="text-orange-600 hover:text-orange-700 font-semibold pl-2"
+                >
+                  Mail
+                </a>
+              </div>
+            </div>
+          </div>
         </form>
       </div>
     </div>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,31 +10,41 @@ const Footer = () => {
     <div className="mt-10 bg-[#101010] p-6 md:p-12">
       <div className="flex flex-row justify-center items-center gap-2">
         <a
-          href="https://twitter.com/harshi_agr"
-          className="border border-gray-600 rounded-full p-2 opacity-50 hover:opacity-100"
-        >
-          <FaTwitter size={20} />
-        </a>
-        <a
           href="https://www.linkedin.com/in/harshi-agrawal-5a638b238/"
+          target="_blank"
+          rel="noopener noreferrer"
           className="border border-gray-600 rounded-full p-2 opacity-50 hover:opacity-100"
         >
           <FaLinkedinIn size={20} />
         </a>
         <a
-          href="https://github.com/harshi706"
+          href="https://github.com/harshi706?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
           className="border border-gray-600 rounded-full p-2 opacity-50 hover:opacity-100"
         >
           <FaGithub size={20} />
         </a>
         <a
           href="https://leetcode.com/harshi_agrawal/"
+          target="_blank"
+          rel="noopener noreferrer"
           className="border border-gray-600 rounded-full p-2 opacity-50 hover:opacity-100"
         >
           <SiLeetcode size={20} />
         </a>
         <a
+          href="https://twitter.com/harshi_agr"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="border border-gray-600 rounded-full p-2 opacity-50 hover:opacity-100"
+        >
+          <FaTwitter size={20} />
+        </a>
+        <a
           href="https://www.instagram.com/harshi_ag._/"
+          target="_blank"
+          rel="noopener noreferrer"
           className="border border-gray-600 rounded-full p-2 opacity-50 hover:opacity-100"
         >
           <FaInstagram size={20} />
